9ef4283872325abd5192dd85ee795882488cbdfb
neryklin <neryklin@mail.ru>
2025-01-29 01:06:39 +0300

    final 10 sprint 2 step

--- src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDate.java
@@ -7,7 +7,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.time.LocalDate;
 
 
 @Target(ElementType.FIELD)
